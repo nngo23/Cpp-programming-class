705e0158080ee4ea5b31866fc4e8c6a407e2e1f5
Ngo Ngoc Sang <ngocsang26889@gmail.com>
2025-09-27 00:13:56 +0300

    edit code

--- Cpp_Task1.cpp
+++ Cpp_Task1.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <tuple>
 using namespace std;
 
 int main() {
-    cout << "Hello from C++!" << endl;
+    vector<string> days = {"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
+    vector<double> temperature = {0,5.457345,11.89078,15.02356,7.7893567,22,26.56};
+    for (int i = 0; i < 7; i++) {
+        cout << "Day of a week and its temperature:" << days[i] << "&" << temperature[i] << endl;
+        if (temperature[i] < 10) {
+            cout << "Cold day" << endl;
+        } else if (temperature[i] <= 10 || temperature[i] >= 20 ) {
+            cout << "Okay day" << endl;
+        } else {
+            cout << "Warm day" << endl;
+        }
+    }
+    //tuple<string,double> warmest_day = {""}
+    
     return 0;
 }
