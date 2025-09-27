c88e6ee81394cb6c959d3a8671314e1d2a8f797b
Ngo Ngoc Sang <ngocsang26889@gmail.com>
2025-09-28 01:27:56 +0300

    change code

--- Cpp_Task1-2.cpp
+++ Cpp_Task1-2.cpp
@@ -1,27 +1,37 @@
 #include <iostream>
 #include <string>
-#include <vector>
-#include <tuple>
+#include <map>
 using namespace std;
 
 int main() {
-    vector<string> days = {"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
-    vector<double> temperature = {0,5.457345,11.89078,15.02356,7.7893567,22,26.56};
-    double max_i = 0;
-    for (int i = 0; i < 7; i++) {
-        cout << "Day in a week and its temperature: " << days[i] << " & " << temperature[i] << endl;
-        if (temperature[i] < 10) {
-            cout << "Cold day" << endl;
-        } else if (temperature[i] >= 10 && temperature[i] <= 20 ) {
-            cout << "Okay day" << endl;
-        } else {
-            cout << "Warm day" << endl;
-        }
-        if (temperature[i] > temperature[max_i]) {
-            max_i = i;
-        }
+    map<string, double> day_temp = {{"Sun", 0}, {"Mon", 5.457345}, {"Tue", 11.89078},
+    {"Wed", 15.02356}, {"Thu", 7.7893567}, {"Fri", 22}, {"Sat", 26.56}};
+    map<string,pair<double,char>> day_weather;
+    day_weather["Sun"].second = 'S';
+    day_weather["Mon"].second = 'C';
+    day_weather["Tue"].second = 'R';
+    day_weather["Wed"].second = 'S';
+    day_weather["Thu"].second = 'C';
+    day_weather["Fri"].second = 'R';
+    day_weather["Sat"].second = 'S';
+    for (const auto & a : day_temp) {
+        day_weather[a.first].first = a.second; 
     }
-    tuple<string,double> warmest_day = {days[max_i],temperature[max_i]};
-    cout << "The warmest day is: " << get<0>(warmest_day) << " with " << get<1>(warmest_day) << " °C" << endl;
+    for (const auto & b : day_temp) {
+        cout << b.first << " : " << b.second << "°C, ";
+        switch (day_weather[b.first].second) {
+            case 'S': cout << "Sunny"; break;
+            case 'C': cout << "Cloudy"; break;
+            case 'R': cout << "Rainy"; break;
+            default: cout << "Unknown";
+        }
+        cout << endl;
+    }
+    double sum = 0;
+    for (const auto & c : day_temp) {
+        sum += c.second;
+    }
+    double average_temp = sum/day_temp.size();
+    cout << "The average temperature is: " << average_temp << " °C" << endl;
     return 0;
 }
