df256e6bd02cc0a6dc8c9c7e681c9957dd6fbca3
Ngo Ngoc Sang <ngocsang26889@gmail.com>
2025-09-28 01:54:45 +0300

    change code

--- Cpp_Task1-1.cpp
+++ Cpp_Task1-1.cpp
@@ -8,8 +8,9 @@ int main() {
     vector<string> days = {"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
     vector<double> temperature = {0,5.457345,11.89078,15.02356,7.7893567,22,26.56};
     double max_i = 0;
+    cout << "Day in a week and its temperature in Celsius degree: " << endl;
     for (int i = 0; i < 7; i++) {
-        cout << "Day in a week and its temperature: " << days[i] << " & " << temperature[i] << endl;
+        cout << days[i] << " & " << temperature[i] << " , ";
         if (temperature[i] < 10) {
             cout << "Cold day" << endl;
         } else if (temperature[i] >= 10 && temperature[i] <= 20 ) {
